2672c380229636f847cefdc8235019a843772cab
jhinx38 <hakimzack42@gmail.com>
2025-06-19 10:37:33 +0800

    Fix Cant load vendor file

--- api/ajax/index.php
+++ api/ajax/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 use PhpOffice\PhpWord\TemplateProcessor;
 
 $reasonLetterPath = '../../src/MS/reason_letter.docx';
